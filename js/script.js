148dc81cb34a8eae26255b8860472dd18dc923ff
FedericoMannelli <134335992+FedericoMannelli@users.noreply.github.com>
2023-06-07 18:05:05 +0200

    commit 2

--- js/script.js
+++ js/script.js
@@ -1,28 +1,53 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+// const inputBox = document.getElementById("input-box");
+// const listContainer = document.getElementById("list-container");
 
-function addTask() {
-    if (inputBox.value === '') {
-        alert("devi scrivere qualcosa");
-    } else {
-        let li = document.createElement("li");
-        li.innerhtml = inputBox.value;
-        listContainer.appendChild(li);
-let span = document.createElement("span");
-span.innerHtml = ""
+// function addTask() {
+//     if (inputBox.value === '') {
+//         alert("devi scrivere qualcosa");
+//     } else {
+//         let li = document.createElement("li");
+//         li.innerhtml = inputBox.value;
+//         listContainer.appendChild(li);
+// let span = document.createElement("span");
+// span.innerHtml = ""
         
-    }
-    inputBox.value = "/00d7";
-    li.appendChild(span);
+//     }
+//     inputBox.value = "/00d7";
+//     li.appendChild(span);
 
     
-}
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
+// }
+// listContainer.addEventListener("click", function(e){
+//     if(e.target.tagName === "LI") {
+//         e.target.classList.toggle("checked");
         
-    } else if(e.target.tagName === "SPAN"){ 
-        e.target.parentElement.remove();
+//     } else if(e.target.tagName === "SPAN"){ 
+//         e.target.parentElement.remove();
         
-    }
-},false);
+//     }
+// },false);
+let contactForm = document.getElementById('main-form')
+contactForm.addEventListener('submit', function (e))  
+    e.preventDefault() 
+
+    const inputBoxInputField = document.getElementById('name')
+ 
+
+  const retrievedInputBox = InputBoxInputField.value
+ 
+  const newTask = {
+    inputBox: retrievedName }
+    console.log('TASK INSERITO', newTask)
+
+    let newImputBoxCard = document.createElement('div')
+    newInputBoxCard.classList.add('InputBox-card')
+
+    newContactCard.innerHTML = `
+    <p>${retrievedName} </p>
+    <button>ELIMINA</button>
+  `
+  const savedContactsDiv = document.getElementById('saved-contacts')
+  savedContactsDiv.appendChild(newContactCard)
+
+
+
